d7dff6382f2545053bb1b03fd6fad8816d6aa6bb
nepp <0andrewmartin0@gmail.com>
2017-01-12 12:08:08 +0000

    blah

--- Summers/Program.fs
+++ Summers/Program.fs
@@ -14,11 +14,9 @@ let b =
    T, cons(car(x), cons(head(tail(x)), cons(head(tail(tail(x))), [])))]
 *)
 
-type sexp =
-  | Cons of value * sexp
-  | Nil
+type sexp = value list
 and value =
-  | Atom of obj
+  | Atom of string
   | Sexp of sexp
 
 type basic_function =
@@ -35,54 +33,34 @@ let parse_sexp (s : string) =
   let rec parse_sexp i : sexp * int =
     // Deals with finding and skipping the first paren
     match cs.[i] with
-    | '(' ->
-      let vals, i = parse_list [] (i + 1)
-      (vals |> List.fold (fun t h -> Cons(h, t)) Nil), i
+    | '(' -> parse_list (i + 1)
     | ' ' -> parse_sexp (i + 1)
     | _ -> failwith "not implemented"
-  and parse_list values i : value list * int =
+  and parse_list i : value list * int =
     // Parses all the things
     match cs.[i] with
     | '(' ->
       let head, i = parse_sexp i
-      parse_list ((Sexp head) :: values) i
+      let tail, i = parse_list i
+      (Sexp head) :: tail, i
     | ')' ->
-      values, i + 1
+      [], i + 1
     | ' ' ->
-      parse_list values (i + 1)
+      parse_list (i + 1)
     | _ ->
       let end_index = parse_word i
-      let head = Atom (cs.[i..end_index] |> System.String)
-      parse_list (head :: values) end_index
-    
+      let head = Atom (cs.[i..end_index-1] |> System.String)
+      let tail, i = parse_list end_index
+      head :: tail, i
+      
   parse_sexp 0
 
-let print_sexp sexp =
-  let l = new System.Collections.Generic.List<string> ()
-  let rec print_list
-  let rec print_sexp sexp =
-    match sexp with
-    | Cons(h, t) ->
-      l.Add "("
-      l.Add ")"
-    | Nil -> l.Add "()"
-  print sexp
-  String.concat "" l
-
-let rec to_expr(ls : obj list) =
-  match ls with
-  | [] -> Nil
-  | x :: xs ->
-    match box x with
-    | :? list<obj> as a ->
-      let v = to_expr a
-      Cons(Sexp v, to_expr xs)
-    | _ -> Cons(Atom x, to_expr xs)
-
-type example = { input : sexp ; output : sexp }
-
-let to_example (input, output) =
-  { input = input |> List.map box |> to_expr ; output = output |> List.map box |> to_expr }
+let rec print_sexp sexp =
+  let print_value v =
+    match v with
+    | Atom s -> s
+    | Sexp s -> print_sexp s
+  sprintf "(%s)" (sexp |> Seq.map print_value |> String.concat " ")
 
 let examples =
   [
@@ -96,12 +74,12 @@ let enumerate_sexps sexp =
   let rec enumerate sexp bf = seq {
     yield Sexp sexp, bf
     match sexp with
-    | Cons(value, rest) ->
+    | value :: rest ->
       yield! enumerate rest (Tail bf)
       match value with
       | Atom a -> yield value, (Head bf)
       | Sexp s -> yield! enumerate s (Head bf)
-    | Nil -> yield Sexp Nil, bf
+    | [] -> yield Sexp [], bf
   }
   enumerate sexp X |> Seq.toArray
   
@@ -109,7 +87,8 @@ let enumerate_sexps sexp =
 
 [<EntryPoint>]
 let main argv =
-  let v, i = parse_sexp "((abba b) (c d))"
-  let s = sprintf "%A" v
+  let sexp, i = parse_sexp "((abba b) (c d))"
+  let all_sexps = enumerate_sexps sexp |> Array.filter (fun (s, _) -> s <> Sexp [])
+  let s = print_sexp sexp
   //let v = to_expr [['a' ; 'b'] ; ['c' ; 'd']]
   0 // return an integer exit code
